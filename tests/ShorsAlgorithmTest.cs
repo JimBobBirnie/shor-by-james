dc5396f4bb91b5796dd3a4985b15e3de0bbcd8db
James Birnie <jimbobbirnie@gmail.com>
2018-11-21 15:59:48 +0000

    changed name of last test to what it actually does

--- tests/ShorsAlgorithmTest.cs
+++ tests/ShorsAlgorithmTest.cs
@@ -88,7 +88,7 @@ namespace ShorByJames
         }
 
         [Fact]
-        public void WhenHalfPeriodExponentIsMinusOneFactoriseGetsADifferentRandomNumber()
+        public void WhenPeriodIsEvenFactoriseChecksHalfPeriod()
         {
             var numberToFactorise = 15;
             _randomNumberHelper.Setup(s => s.GetRandomGreaterThanTwoLessThanN(numberToFactorise, null))
