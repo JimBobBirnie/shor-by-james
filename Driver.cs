13603e468ef5f02abbf315ec1a428813e2952f98
James Birnie <jimbobbirnie@gmail.com>
2018-11-23 14:03:32 +0200

    removed args because I can't work out how to pass them and I don't have time

--- Driver.cs
+++ Driver.cs
@@ -9,25 +9,8 @@ namespace ShorByJames
     {
         static void Main(string[] args)
         {
-            int numberToFactorise = 0;
-            if (args.Length == 0)
-            {
-                numberToFactorise = 15;
-            }
-            else
-            {
-                int.TryParse(args[0], out numberToFactorise);
-            }
-            if (numberToFactorise == 0)
-            {
-                Console.WriteLine("no number to factorise");
-            }
-            else
-            {
-                var factoriser = new Factoriser(new RandomNumberHelper(), new ModularExponentHelper());
-                factoriser.Factorise(15);
-            }
-
+            var factoriser = new Factoriser(new RandomNumberHelper(), new ModularExponentHelper());
+            factoriser.Factorise(15);
         }
     }
 }
